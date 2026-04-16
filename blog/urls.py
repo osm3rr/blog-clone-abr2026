235d30eb2e0df4ac40b6f643ceef07cdbad50f03
osm3rr <osm3rr@adakademy.com>
2026-04-16 19:53:54 -0400

    detail view done

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (ArticleListView,
+                    ArticleDetailView)
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
+    path('articulos/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 
 
@@ -6,3 +6,7 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = 'articles_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles_detail.html'
